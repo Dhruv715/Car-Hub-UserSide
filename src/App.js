5ec0381267fbc5a6574aa7bb3b3a614adabf4cff
Dhruv <124017998+Dhruv715@users.noreply.github.com>
2024-06-18 10:46:36 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './components/Home';
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, useLocation } from "react-router-dom";
 import About from './components/About';
 import Contact from './components/Contact';
 import Cars from './components/Cars';
@@ -11,26 +11,24 @@ import Login from './components/Login';
 import Profile from './components/Profile';
 
 function App() {
+  const location = useLocation();
+
+  // Check if current route is /login or /signup
+  const isAuthRoute = location.pathname === '/login' || location.pathname === '/signup';
+
   return (
     <>
-      <Navbar/>
+      {!isAuthRoute && <Navbar />}
       <Routes>
-      {/* HomePage */}
-        <Route path="/" element={ <Home/> } />
-        {/* About Page */}
-        <Route path="/about" element={ <About/> } />
-        {/* Contact Us Page */}
-        <Route path="/contact" element={ <Contact/> } />
-        {/* Catelog Page */}
-        <Route path="/cateloge" element={ <Cars/> } />
-        {/* Login */}
-        <Route path="/login" element={ <Login/> } />
-        {/* Signup Form */}
-        <Route path="/Signup" element={ <Signup/> } />
-        {/* Profile */}
-        <Route path="/Profile" element={ <Profile/> } />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/catalogue" element={<Cars />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
-      <Footer/>
+      {!isAuthRoute && <Footer />}
     </>
   );
 }
